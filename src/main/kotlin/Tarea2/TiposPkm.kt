8c8ddb36458350e39d5407072bafbeb6a2b0f7a7
Orlando Cavieres <frizortake@gmail.com>
2020-08-08 02:25:54 -0400

    Completada clase GroundPokemon

--- src/main/kotlin/Tarea2/TiposPkm.kt
+++ src/main/kotlin/Tarea2/TiposPkm.kt
@@ -131,8 +131,37 @@ class PsychicPokemon(override val nombre: String): Pokemon {
 class GroundPokemon(override val nombre: String): Pokemon {
     override var contadorDamage = 0
     override val tipo = "Tierra"
-    override val debilidad = listOf("Agua", "Hierba")
-    override val fortaleza = listOf("Normal", "Electrico")
+    override var ataqueSeleccionado = 1
+
+    override fun atacarContrincanteDefinido(pokemon: Pokemon): Pokemon {
+        return pokemon.atacar(this)
+    }
+
+    override fun realizarAtaque(oponente: Pokemon, ataque: Int) {
+        when(ataque) {
+            1 -> oponente.recibeGroundDamage(this)
+            2 -> oponente.recibeNormalDamage(this)
+        }
+    }
+
+    override fun recibeNormalDamage(oponente: Pokemon) {
+        when (oponente.ataqueSeleccionado) {
+            1 -> this.contadorDamage += oponente.ataquePrimario - 20
+            2 -> this.contadorDamage += oponente.ataqueSecundario - 20
+        }
+    }
+
+    override fun recibeWaterDamage(oponente: WaterPokemon) {
+        modDamageAtaquePrimario(pokemon = oponente, modificador = 20)
+    }
+
+    override fun recibeElectricDamage(oponente: ElectricPokemon) {
+        modDamageAtaquePrimario(pokemon = oponente, modificador = -20)
+    }
+
+    override fun recibeGrassDamage(oponente: GrassPokemon) {
+        modDamageAtaquePrimario(pokemon = oponente, modificador = 20)
+    }
 }
 
 class FightPokemon(override val nombre: String): Pokemon {
