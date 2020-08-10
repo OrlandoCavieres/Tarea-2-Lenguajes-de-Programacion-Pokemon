bc75fe235d1d9eb7fd7ab0287b638a000b0556da
Orlando Cavieres <frizortake@gmail.com>
2020-08-10 06:08:56 -0400

    Cambios realizado de nombre val equipoPKM a equipoPokemon. Corregido un error en interface con preferenciaTipoPokemon

--- src/main/kotlin/Tarea2/Entrenador.kt
+++ src/main/kotlin/Tarea2/Entrenador.kt
@@ -2,7 +2,8 @@ package Tarea2
 
 interface Entrenador {
     val nombre: String
-    val equipoPKM: MutableList<Pokemon>
-    val totalPokemonEnEquipo: Int get() = this.equipoPKM.size
+    val equipoPokemon: MutableList<Pokemon>
+    val totalPokemonEnEquipo: Int get() = this.equipoPokemon.size
     val tipo: String get() = "JUGADOR"
+    val preferenciaTipoPokemon: List<String> get() = listOf("Normal")
 }
--- src/main/kotlin/Tarea2/Jugador.kt
+++ src/main/kotlin/Tarea2/Jugador.kt
@@ -1,28 +1,28 @@
 package Tarea2
 
 class Jugador(override val nombre: String): Entrenador {
-    override val equipoPKM: MutableList<Pokemon> = mutableListOf()
+    override val equipoPokemon: MutableList<Pokemon> = mutableListOf()
     private var dinero = 3000
     private var totalPokemonCapturados = 0
 
     protected fun elegirPrimerPokemon(tipo: String) {
         when {
-            this.equipoPKM.isEmpty() -> this.pokemonInicial(tipo)
+            this.equipoPokemon.isEmpty() -> this.pokemonInicial(tipo)
             else -> println("Ya se ha elegido el primer Pokemon")
         }
     }
 
     private fun pokemonInicial(tipo: String) {
         when (tipo) {
-            "Hierba" -> this.equipoPKM.add(GrassPokemon(nombre = "Bulbasaur", vida = 45, ataquePrimario = 65, ataqueSecundario = 49))
-            "Fuego" -> this.equipoPKM.add(FirePokemon(nombre = "Charmander", vida = 39, ataquePrimario = 60, ataqueSecundario = 52))
-            "Agua" -> this.equipoPKM.add(WaterPokemon(nombre = "Squirtle", vida = 44, ataquePrimario = 50, ataqueSecundario = 48))
+            "Hierba" -> this.equipoPokemon.add(GrassPokemon(nombre = "Bulbasaur", vida = 45, ataquePrimario = 65, ataqueSecundario = 49))
+            "Fuego" -> this.equipoPokemon.add(FirePokemon(nombre = "Charmander", vida = 39, ataquePrimario = 60, ataqueSecundario = 52))
+            "Agua" -> this.equipoPokemon.add(WaterPokemon(nombre = "Squirtle", vida = 44, ataquePrimario = 50, ataqueSecundario = 48))
         }
         this.totalPokemonCapturados += 1
     }
 
     protected fun verificarCupoMaxEquipoPKM(): Boolean {
-        return this.equipoPKM.size <= 6
+        return this.equipoPokemon.size <= 6
     }
 
     fun mostrarTotalPokemonCapturados(): Int {
