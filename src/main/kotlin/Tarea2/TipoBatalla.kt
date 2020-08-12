5249275f864c752d759189e559ee51a2a29dcc93
Orlando Cavieres <frizortake@gmail.com>
2020-08-12 17:53:06 -0400

    Cambios en el orden de variables, eliminacion de variables rebundantes y reorganización de texto en Tipos de Batalla.

--- src/main/kotlin/Tarea2/TipoBatalla.kt
+++ src/main/kotlin/Tarea2/TipoBatalla.kt
@@ -57,8 +57,7 @@ class BatallaContraPokemonSalvaje(override val jugador: Jugador, override val sa
     }
 
     override fun capturarPokemon() {
-        val numAzar = (0..100).toList().random()
-        when (numAzar) {
+        when ((0..100).toList().random()) {
             0 -> println("No se puede capturar un pokemon debilitado.")
             in 1..porcentajeProbabilidadCaptura() -> this.finBatalla("Captura")
             else -> {println("¡Oh, mala suerte, ${this.salvaje.nombre} no ha sido capturado!")
@@ -68,26 +67,28 @@ class BatallaContraPokemonSalvaje(override val jugador: Jugador, override val sa
     }
 
     override fun deseaPonerApodo(decision: Boolean, nuevoApodo: String) {
-        when (decision) {
-            true -> this.salvaje.ponerApodo(nuevoApodo)
-            false -> this.salvaje.ponerApodo(this.salvaje.nombre)
+        if (this.condicionFinalBatalla == "Captura") {
+            when (decision) {
+                true -> this.salvaje.ponerApodo(nuevoApodo)
+                false -> this.salvaje.ponerApodo(this.salvaje.nombre)
+            }
         }
+        else {println("No hay pokemon capturado al que colocar apodo.")}
     }
 
     override fun addPokemonCapturado() {
         if (this.condicionFinalBatalla == "Captura") {
             this.jugador.addPokemon(this.salvaje)
         }
-        else {println("No se puede capturar al pokemon")}
+        else {println("No se puede capturar al pokemon.")}
     }
 
     override fun probabilidadEscape() {
-        val eleccionRandom = listOf(1, 2, 3, 4, 5).random()
-        when (eleccionRandom) {
+        when (listOf(1, 2, 3, 4, 5).random()) {
             3 -> {println("No has logrado escapar")
                   this.turnoProgresoBatalla += 1
                   this.cambiarParticipanteActivo()}
-            1, 2, 4, 5 -> {println("Has escapado sin problemas")
+            1, 2, 4, 5 -> {println("Has escapado sin problemas.")
                            this.condicionFinalBatalla = "Escape"}
         }
     }
@@ -110,7 +111,7 @@ class BatallaContraPokemonSalvaje(override val jugador: Jugador, override val sa
     }
 }
 
-class BatallaContraOtroEntrenador(override val jugador: Jugador, override val oponente: Entrenador): Batalla {
+open class BatallaContraOtroEntrenador(override val jugador: Jugador, override val oponente: Entrenador): Batalla {
     override var participanteActivo = 1
     override var turnoProgresoBatalla = 0
     override var condicionFinalBatalla = ""
@@ -175,7 +176,7 @@ class BatallaContraOtroEntrenador(override val jugador: Jugador, override val op
         return cuentaVencidos == this.oponente.totalPokemonEnEquipo
     }
 
-    protected fun mensajeDerrotaPkmOponente(): String {
+    private fun mensajeDerrotaPkmOponente(): String {
         return "¡El ${this.oponente.recuperarPokemonEnCabecera()!!.nombre} de ${this.oponente.tipoClaseEntrenador} ${this.oponente.nombre.toUpperCase()} se ha debilitado!"
     }
 
