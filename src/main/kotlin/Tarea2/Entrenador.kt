61a6ed5e9033df599794a8b56533ab171cf7e875
Orlando Cavieres <frizortake@gmail.com>
2020-08-12 03:09:15 -0400

    Creados métodos para generar un equipo pokemon aleatorio en base a la cantidad minima de pokemon de cada tipo entrenador y sus preferencias de tipo pokemon.

--- src/main/kotlin/Tarea2/Entrenador.kt
+++ src/main/kotlin/Tarea2/Entrenador.kt
@@ -12,6 +12,7 @@ interface Entrenador {
     val sizeMinimoEquipoPokemon: Int get() = 1
     val mensajeInicial: String get() = "JUGADOR"
     val mensajeDerrota: String get() = "JUGADOR"
+    val dineroRecompensa: Int get() = 0
 
     fun recuperarPokemonEnLaPosicionEquipo(posicion: Int): Pokemon? {
         val nombrePKM = this.ordenEquipoPokemon[posicion]
@@ -25,5 +26,65 @@ interface Entrenador {
         return recuperarPokemonEnLaPosicionEquipo(0)
     }
 
+    fun obtenerPkmNoDerrotadoACabecera() {
+        for (posicion in 0..this.totalPokemonEnEquipo) {
+            if (this.equipoPokemon[this.ordenEquipoPokemon[posicion]]!!.fueraDeCombate()) {
+                this.cambiarOrdenPokemonEnEquipo(posicion, 0)
+                break
+            }
+        }
+    }
+
+    fun cambiarOrdenPokemonEnEquipo(posicionActual: Int, posicionNueva: Int) {
+        val pkmEnPosActual = this.ordenEquipoPokemon[posicionActual]
+        val pkmEnPosNueva = this.ordenEquipoPokemon[posicionNueva]
+        this.ordenEquipoPokemon[posicionActual] = pkmEnPosNueva
+        this.ordenEquipoPokemon[posicionNueva] = pkmEnPosActual
+    }
+
     fun confirmarBatalla(batallaPKM: Batalla): Entrenador
+
+    fun generarEquipoPokemon() {
+        for (elemento in 0..this.sizeMinimoEquipoPokemon) {
+            val tipoNuevoPKM = this.preferenciaTipoPokemon.random()
+            val nuevoPKM = this.crearPokemon(tipoNuevoPKM)
+            val nivelPKM = ((this.nivelAproximado - 3)..(this.nivelAproximado + 2)).random()
+            while (nuevoPKM!!.nivel < nivelPKM) {
+                nuevoPKM.experienciaAcumuladaEnNivel += nuevoPKM.experienciaRequeridaSubirNivel()
+                nuevoPKM.subirNivel()
+            }
+            this.equipoPokemon[elemento] = nuevoPKM
+            this.ordenEquipoPokemon.add(elemento)
+        }
+    }
+
+    fun crearPokemon(tipoPKM: String): Pokemon? {
+        val nombrePKMNormal = listOf("Rattata", "Meowth", "Chansey", "Tauros", "Ditto",
+                                     "Eevee", "Snorlax", "Dunsparce", "Pidgey", "Jigglypuff")
+        val nombrePKMFuego = listOf("Charmander", "Growlithe", "Vulpix", "Cyndaquil", "Ponyta",
+                                    "Magmar", "Slugma", "Flareon", "Torchic", "Torkoal")
+        val nombrePKMAgua = listOf("Squirtle", "Psyduck", "Poliwag", "Seel", "Krabby",
+                                   "Horsea", "Magikarp", "Staryu", "Goldeen", "Mudkip")
+        val nombrePKMElectrico = listOf("Pikachu", "Voltorb", "Electabuzz", "Mareep", "Electrike",
+                                        "Magnemite", "Blitzle", "Shinx", "Minun", "Jolteon")
+        val nombrePKMPsiquico = listOf("Abra", "Slowpoke", "Hypno", "Espeon", "Unown",
+                                       "Mr Mime", "Natu", "Ralts", "Jynx", "Lunatone")
+        val nombrePKMTierra = listOf("Sandslash", "Diglett", "Cubone", "Phanpy", "Trapinch",
+                                     "Rhyhorn", "Gligar", "Baltoy", "Geodude", "Onyx")
+        val nombrePKMLucha = listOf("Mankey", "Machop", "Hitmonlee", "Hitmonchan", "Tyrogue",
+                                    "Hitmontop", "Makuhita", "Riolu", "Meditate", "Lucario")
+        val nombrePKMHierba = listOf("Tangela", "Chikorita", "Bellosom", "Oddish", "Bellsprout",
+                                     "Exeggcute", "Tropius", "Snivy", "Treecko", "Turtwig")
+        when (tipoPKM) {
+            "Normal" -> return NormalPokemon(nombre = nombrePKMNormal.random())
+            "Fuego" -> return FirePokemon(nombre = nombrePKMFuego.random())
+            "Agua" -> return FirePokemon(nombre = nombrePKMAgua.random())
+            "Electrico" -> return FirePokemon(nombre = nombrePKMElectrico.random())
+            "Psiquico" -> return FirePokemon(nombre = nombrePKMPsiquico.random())
+            "Tierra" -> return FirePokemon(nombre = nombrePKMTierra.random())
+            "Lucha" -> return FirePokemon(nombre = nombrePKMLucha.random())
+            "Hierba" -> return FirePokemon(nombre = nombrePKMHierba.random())
+            else -> return null
+        }
+    }
 }
