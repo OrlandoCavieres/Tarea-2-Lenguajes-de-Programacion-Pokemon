d54a57f94ab5460a57cbeaf60c217bc84ba74a0f
Orlando Cavieres <frizortake@gmail.com>
2020-08-12 17:54:51 -0400

    Documentacion de las interfaces Pokemon y Batalla.

--- src/main/kotlin/Tarea2/Batalla.kt
+++ src/main/kotlin/Tarea2/Batalla.kt
@@ -9,10 +9,16 @@ interface Batalla {
     var turnoProgresoBatalla: Int
     var condicionFinalBatalla: String
 
+    /**
+     * Método que determina de forma random que participante iniciará la batalla.
+     */
     fun quienEmpieza() {
         this.participanteActivo = listOf(1, 2).random()
     }
 
+    /**
+     * Método cambia el participante activo actual. Intercambia entre 1 y 2 siempre.
+     */
     fun cambiarParticipanteActivo() {
         when (this.participanteActivo) {
             1 -> this.participanteActivo = 2
@@ -20,27 +26,90 @@ interface Batalla {
         }
     }
 
+    /**
+     * Método que le solicita al jugador sus datos para la batalla.
+     * @param jugador Jugador participante en la batalla
+     * @return Retorna al jugador después de que éste entregue sus datos.
+     */
     fun confirmarJugador(jugador: Jugador): Jugador {
         return jugador.confirmarBatalla(this)
     }
 
+    /**
+     * Método que le solicita al pokemon salvaje sus datos para la batalla.
+     * @param pkmSalvaje Pokemon salvaje participante en la batalla
+     * @return Retorna al pokemon salvaje después de que éste entregue sus datos.
+     */
     fun confirmarOponente(pkmSalvaje: Pokemon): Pokemon {
         return pkmSalvaje.confirmarBatallaPokemonSalvaje(this)
     }
 
+    /**
+     * Método que le solicita al entrenador contrincante sus datos para la batalla.
+     * @param contrincante Entrenador contrincante participante en la batalla
+     * @return Retorna al entrenador contrincante después de que este entregue sus datos.
+     */
     fun confirmarOponente(contrincante: Entrenador): Entrenador {
         return contrincante.confirmarBatalla(this)
     }
 
+    /**
+     * Método que muestra el mensaje inicial de la batalla.
+     * @return Mensaje inicial como string.
+     */
     fun mensajeInicioBatalla(): String
+
+    /**
+     * Método que determina como se desarrollará el primer turno de la batalla pokemon.
+     */
     fun primerTurno()
+
+    /**
+     * Método que lleva a cabo el turno del jugador. En el transcurso del mismo, decide si ha vencido a uno o a todos
+     * los pokemon del contricante, determinado la derrota de este último si es necesario. Solicita cambiar de
+     * pokemon al contrincante, si se vence al pokemon activo de éste. Otorga experiencia al pokemon activo del jugador
+     * si en el trancurso del turno vence a un pokemon del oponente.
+     * @param jugador Corresponde al jugador.
+     */
     fun turnoAtaqueJugador(jugador: Jugador)
+
+    /**
+     * Método que lleva a cabo el turno del pokemon salvaje oponente. En el transcurso del mismo, decide si ha vencido
+     * a uno o a todos los pokemon del jugador, determinado la derrota de este último si es necesario. Solicita cambiar de
+     * pokemon al jugador si se vence al pokemon activo de éste.
+     * @param pkmSalvaje Corresponde al pokemon salvaje contrincante de la batalla.
+     */
     fun turnoAtaqueOponente(pkmSalvaje: Pokemon)
+
+    /**
+     * Método que lleva a cabo el turno del entrenador oponente. En el transcurso del mismo, decide si ha vencido a uno
+     * o a todos los pokemon del jugador, determinado la derrota de este último si es necesario. Solicita cambiar de
+     * pokemon al jugador si se vence al pokemon activo de éste.
+     * @param entrenador Corresponde al entrenador contrincante de la batalla.
+     */
     fun turnoAtaqueOponente(entrenador: Entrenador)
 
+    /**
+     * Método que desarrolla el turno con la opción atacar, considerando al participante activo en ese momento para
+     * establecer si es turno del jugador o del pokemon salvaje.
+     * @param jugador Corresponde al jugador.
+     * @param oponente Corresponde al pokemon salvaje.
+     */
     fun desarrolloTurnoAtaque(jugador: Jugador, oponente: Pokemon)
+
+    /**
+     * Método que desarrolla el turno con la opción atacar, considerando al participante activo en ese momento para
+     * establecer si es turno del jugador o del entrenador oponente.
+     * @param jugador Corresponde al jugador.
+     * @param oponente Corresponde al entrenador contrincante.
+     */
     fun desarrolloTurnoAtaque(jugador: Jugador, oponente: Entrenador)
 
+    /**
+     * Método que verifica si el jugador fue derrotado, al revisar que todos los pokemon en el equipo estén fuera de
+     * combate.
+     * @return True si todos los pokemon del jugador en equipo están fuera de combate.
+     */
     fun verificarDerrotaJugador(): Boolean {
         var cuentaVencidos = 0
         for (pkm in confirmarJugador(this.jugador).equipoPokemon.values) {
@@ -51,9 +120,24 @@ interface Batalla {
         return cuentaVencidos == this.jugador.totalPokemonEnEquipo
     }
 
+    /**
+     * Método que verifica si el oponente fue derrotado, al revisar que todos los pokemon participantes estén fuera de
+     * combate.
+     * @return True si todos los pokemon oponentes están fuera de combate.
+     */
     fun verificarDerrotaOponente(): Boolean
+
+    /**
+     * Método que implementa la captura de un pokemon, en base a un porcentaje de exito de captura y un controlador
+     * random. Si hay éxito, entonces actualiza la condición de la batalla. Caso contrario, no se logra la captura y
+     * el jugador pierde su turno.
+     */
     fun capturarPokemon()
 
+    /**
+     * Método que entrega el porcentaje de posibilidad de captura de un pokemon en base a su vida restante.
+     * @return Porcentaje de éxito de captura del pokemon.
+     */
     fun porcentajeProbabilidadCaptura(): Int {
         return when {
             this.salvaje.vidaRestante() == this.salvaje.vida -> 30
@@ -65,14 +149,53 @@ interface Batalla {
         }
     }
 
+    /**
+     * Método que permite añadir un pokemon capturado al equipo pokemon del jugador
+     */
     fun addPokemonCapturado()
+
+    /**
+     * Método que permite ponerle un apodo a un pokemon capturado por el jugador.
+     * @param decision True si desea ponerle un apodo, false caso contrario.
+     * @param nuevoApodo Apodo que se desea colocar al pokemon.
+     */
     fun deseaPonerApodo(decision: Boolean, nuevoApodo: String)
+
+    /**
+     * Método que permite al jugador en la batalla, escapar de la misma, empleando el método probabilidadEscape, para
+     * ver si realmente puede escapar o no. Si lo logra genera una condición de salida de batalla, caso contrario el
+     * jugador pierde su turno.
+     * @param jugador Corresponde al jugador que desea escapar.
+     */
     fun escaparBatalla(jugador: Jugador)
+
+    /**
+     * Método que calcula en terminos de intervalos la probabilidad de que el jugador escape de una batalla pokemon.
+     */
     fun probabilidadEscape()
+
+    /**
+     * Retorna el mensaje de derrota del oponente actual.
+     * @return String del mensaje de derrota
+     */
     fun mensajeDerrotaOponente(): String
+
+    /**
+     * Método que establece las ganancias para el jugador.
+     */
     fun resultadoVictoriaJugador()
+
+    /**
+     * Método que le otorga la experiencia ganada por derrotar a un pokemon rival al pokemon que lo venció.
+     * @param pokemon Pokemon que vence y que recibe experiencia.
+     */
     fun darExperienciaPKM(pokemon: Pokemon) {}
 
+    /**
+     * Método que establece los resultados finales de la batalla, para una condición de salida de la misma
+     * correspondiente.
+     * @param opcion String que representa la claúsula de salida de la batalla.
+     */
     fun finBatalla(opcion: String) {
         when (opcion) {
             "Escape" -> this.probabilidadEscape()
--- src/main/kotlin/Tarea2/Pokemon.kt
+++ src/main/kotlin/Tarea2/Pokemon.kt
@@ -1,8 +1,27 @@
 package Tarea2
 import kotlin.math.pow
 
+/**
+ * Función infija que permite emplear 'elevado' como conector entre dos elementos en una expresión y calcula
+ * la potencia del primer numero con exponente el segundo numero.
+ * @Sample 4 elevado 7
+ */
 infix fun Int.elevado(exponente: Int): Int = toDouble().pow(exponente).toInt()
 
+/**
+ * Interface que representa a un pokemon y sus atributos básicos, así como sus acciones básicas y formas de implementar
+ * y manejarlas.
+ * @property nombre Nombre del pokemon.
+ * @property apodo Apodo ingresado para el pokemon.
+ * @property vida Cantidad de vida disponible del pokemon.
+ * @property tipo Tipo del pokemon en forma string. Por default es Normal.
+ * @property contadorDamage Numero que representa el daño actual acumulado del pokemon
+ * @property ataquePrimario Numero que representa el daño que realiza el primer ataque del pokemon.
+ * @property ataqueSecundario Numero que representa el daño que realiza el segundo ataque del pokemon.
+ * @property ataqueSeleccionado Numero que representa el ataque seleccionado a realizar del pokemon.
+ * @property nivel Numero que representa el nivel actual del pokemon.
+ * @property experienciaAcumuladaEnNivel Numero que representa la cantidad de experiencia acumulada en el nivel actual.
+ */
 interface Pokemon {
     val nombre: String
     var apodo: String
@@ -15,24 +34,39 @@ interface Pokemon {
     var nivel: Int
     var experienciaAcumuladaEnNivel: Int
 
+    /**
+     * Método que permite cambiar el apodo del pokemon.
+     * @param nuevoApodo String con el nuevo apodo.
+     *  */
     fun ponerApodo(nuevoApodo: String) {
         this.apodo = nuevoApodo
     }
 
+    /**
+     * Método que hace que el pokemon inicie el ataque contra otro pokemon, presentándose a sí mismo con él.
+     * @param pokemon Pokemon al que se desea atacar.
+     * @return El pokemon que se desea atacar o null, si realmente no es pokemon.
+     */
     fun iniciarAtaque(pokemon: Pokemon?): Pokemon? {
-        if (pokemon != null) {
+        return if (pokemon != null) {
             pokemon.observarOponenteYEntregarDatos(this)
-            return pokemon
+            pokemon
         }
-        else {
-            return null
-        }
+        else { null }
     }
 
+    /**
+     * Método que le indica a un pokemon que se le permite atacar al pokemon actual.
+     * @param pokemon Pokemon oponente.
+     */
     fun observarOponenteYEntregarDatos(pokemon: Pokemon) {
         pokemon.atacar(this)
     }
 
+    /**
+     * Método que se encarga de ver si es posible atacar al pokemon oponente o no, dadas distintas posibilidades.
+     * @param oponente Pokemon oponente al que se planea atacar.
+     */
     fun atacar(oponente: Pokemon) {
         when {
             this.fueraDeCombate() -> println("${this.nombre}: No puedo atacar, fui derrotado")
@@ -41,12 +75,27 @@ interface Pokemon {
         }
     }
 
+    /**
+     * Método que permite seleccionar entre los distintos ataques del pokemon. Debe ser 1 o 2, dado que solo se tiene
+     * ataque primario y secundario.
+     * @param decision Numero que representa el ataque a realizar. Puede ser 1 o 2.
+     */
     fun seleccionarAtaqueAUsar(decision: Int) {
         this.ataqueSeleccionado = decision
     }
 
+    /**
+     * Método que realiza el ataque estableciendo la ruta según el ataque seleccionado.
+     * @param oponente Pokemon al que se está atacando.
+     * @param ataque Ataque seleccionado. Puede ser 1 o 2.
+     */
     fun realizarAtaque(oponente: Pokemon, ataque: Int)
 
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo normal.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeNormalDamage(oponente: Pokemon) {
         when (oponente.ataqueSeleccionado) {
             1 -> this.contadorDamage += oponente.ataquePrimario
@@ -54,18 +103,68 @@ interface Pokemon {
         }
     }
 
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo fuego.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeFireDamage(oponente: FirePokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo agua.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeWaterDamage(oponente: WaterPokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo electrico.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeElectricDamage(oponente: ElectricPokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo psiquico.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibePsychicDamage(oponente: PsychicPokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo tierra.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeGroundDamage(oponente: GroundPokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo lucha.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeFightDamage(oponente: FightPokemon) {this.recibeNormalDamage(oponente)}
+
+    /**
+     * Método que aumenta el contador de daño del pokemon si el ataque recibido del pokemon contrincante es de
+     * tipo hierba.
+     * @param oponente Pokemon que realiza el ataque.
+     */
     fun recibeGrassDamage(oponente: GrassPokemon) {this.recibeNormalDamage(oponente)}
 
+    /**
+     * Método que verifica si un pokemon está debilitado o no.
+     * @return True si el pokemon está derrotado.
+     */
     fun fueraDeCombate(): Boolean {
         return this.contadorDamage >= this.vida
     }
 
+    /**
+     * Método que calcula la cantidad restante de vida, al considerar la vida base y el daño acumulado en el contador
+     * de daño.
+     * @return Número de vida restante.
+     */
     fun vidaRestante(): Int {
         return when {
             this.contadorDamage >= this.vida -> 0
@@ -73,27 +172,49 @@ interface Pokemon {
         }
     }
 
+    /**
+     * Método que permite restaurar al pokemon a su estado inicial sin daño al resetear su contador de daño.
+     */
     fun restaurarPkmAlMaximo() {
         this.contadorDamage = 0
     }
 
+    /**
+     * Método que permite modificar el daño del ataque primario en base a un modificador dado, el que se suma al daño
+     * base del ataque primario.
+     * @param pokemon Pokemon que realiza el ataque.
+     * @param modificador Número en que se modifica el ataque primario del pokemon.
+     */
     fun modDamageAtaquePrimario(pokemon: Pokemon, modificador: Int) {
         when (pokemon.ataqueSeleccionado) {
             1 -> this.contadorDamage += pokemon.ataquePrimario + modificador
         }
     }
 
+    /**
+     * Método que calcula la cantidad de experiencia necesaria para subir de nivel dado el nivel actual.
+     * @return Número que representa cantidad requerida de experiencia para subir de nivel.
+     */
     fun experienciaRequeridaSubirNivel(): Int {
         return ((this.nivel + 1) elevado 3) - (this.nivel elevado 3)
     }
 
+    /**
+     * Método que verifica si el pokemon está capacitado en experiencia para subir de nivel.
+     * @return True si el pokemon esta habilitado para poder subir de nivel.
+     */
     fun verificarRequisitoExpSubirNivel(): Boolean {
         return this.experienciaAcumuladaEnNivel >= this.experienciaRequeridaSubirNivel()
     }
 
+    /**
+     * Método que permite subir de nivel a un pokemon. Emplea otros métodos y criterios para ver si es posible subir
+     * de nivel en base a la experiencia acumulada y necesaria. Modifica las propiedades del pokemon para ser adaptados al
+     * nuevo nivel.
+     */
     fun subirNivel() {
         when {
-            verificarRequisitoExpSubirNivel() && this.nivel < 100 && this.nivel >= 1 -> {
+            verificarRequisitoExpSubirNivel() && this.nivel >= 1 && this.nivel < 100 -> {
                 this.experienciaAcumuladaEnNivel = this.experienciaAcumuladaEnNivel - this.experienciaRequeridaSubirNivel()
                 this.nivel += 1
                 println("¡${this.nombre} ha subido a nivel ${this.nivel}!")
@@ -107,10 +228,19 @@ interface Pokemon {
         }
     }
 
+    /**
+     * Método que confirma la participación del pokemon en una batalla pokemon, si es un pokemon salvaje.
+     * @param batallaPKM Batalla en la que va a participar el pokemon.
+     * @return Entrega al pokemon.
+     */
     fun confirmarBatallaPokemonSalvaje(batallaPKM: Batalla): Pokemon {
         return this
     }
 
+    /**
+     * Método que permite obtener experiencia al pokemon como resultado de una batalla pokemon.
+     * @param batallaPKM Batalla en la que participa el pokemon.
+     */
     fun recibeExperiencia(batallaPKM: Batalla) {
         batallaPKM.darExperienciaPKM(this)
     }
