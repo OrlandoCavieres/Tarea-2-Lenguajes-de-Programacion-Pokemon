3577853758256328f15bfc34522ea38c1002a68c
Orlando Cavieres <frizortake@gmail.com>
2020-09-01 12:37:11 -0400

    Separada funcion infija elevado en un nuevo script dedicado a  funciones auxiliares.

--- src/main/kotlin/tarea2/Pokemon.kt
+++ src/main/kotlin/tarea2/Pokemon.kt
@@ -1,12 +1,4 @@
 package tarea2
-import kotlin.math.pow
-
-/**
- * Función infija que permite emplear 'elevado' como conector entre dos elementos en una expresión y calcula
- * la potencia del primer numero con exponente el segundo numero.
- * @Sample 4 elevado 7
- */
-infix fun Int.elevado(exponente: Int): Int = toDouble().pow(exponente).toInt()
 
 /**
  * Interface que representa a un pokemon y sus atributos básicos, así como sus acciones básicas y formas de implementar
